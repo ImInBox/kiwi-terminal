071f8ba14b1ac6a0e97ed826458dfb79f7ec741f
I'm In Box <iminbox24dev@gmail.com>
2022-10-16 16:31:19 +0200

    terminal prototype

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 function App() {
+  const [ input, setInput ] = useState("");
+  const [ output, setOutput ] = useState("");
   return (
-    <>
-      <div></div>
-      <input type="text"/>
-    </>
+    <div className="bg-slate-800 text-white h-[100%] box-border p-2">
+      <div>
+        <pre className="text-[15px]">
+          {output}
+        </pre>
+      </div>
+      <div className="flex">
+      <pre className="text-[15px]">$ </pre>
+        <input type="text" value={input} onChange={e=>setInput(e.target.value)} onKeyDown={e=>{
+          if (e.key === "Enter") {
+            let newOutput = "";
+            newOutput = `${output}\n$ ${input}\n`
+
+            switch (input) {
+              case "help":
+                newOutput += "help - display commands list\nclear - clear terminal";
+                break
+              case "clear":
+                setOutput("");
+                break
+              default:
+                newOutput += "Unknow command"
+            }
+
+            setOutput(newOutput)
+            setInput("")
+          }
+        }} className="bg-transparent outline-none m-0 p-0 text-[15px]"/>
+      </div>
+    </div>
   );
 }
 
