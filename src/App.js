8360cedaeca4d41dc2738d8878cbfaa454a7f9b7
I'm In Box <iminbox24dev@gmail.com>
2022-10-30 00:13:29 +0200

    added scroll style, auto scroll and auto input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Baner, Help } from "./components/Responses";
 import Prefix from "./components/Prefix";
 
 function App() {
-  const commands = ["help", "clear", "author", "font"]
+  const commands = ["", "help", "clear", "author", "font"]
+
+  const autoScrollRef = useRef()
+  const inputRef = useRef()
 
   const [ input, setInput ] = useState("");
   const [ output, setOutput ] = useState([{prefix: "", command: "", response: <Baner/>, id: 0}]);
@@ -12,58 +15,69 @@ function App() {
   let newOutput = {}
   let clear = false
 
+  useEffect(() => {
+    autoScrollRef.current.scrollIntoView({ behavior: "smooth" })
+  }, [output])
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
   return (
-    <div className="bg-[#242d3c] box-border p-2 overflow-hidden h-[100vh] w-[100vw]">
-      <div>
-        <pre className="text-[15px]">
-          {output.map((output) => <div key={output.id} className="h-full w-full font-JetBrainsMono">
-            <p>
-              {output.prefix}
-              <span className="text-[#cbc57e]">
-                {output.command}
-              </span>
-            </p>
-            <div>
-              {output.response}
-            </div>
-          </div>)}
+    <div className="bg-[#242d3c] box-border h-screen p-2" onClick={e=>inputRef.current.focus()}>
+      <div className="overflow-y-scroll scrollbar w-full sm:h-full">
+        <div>
+          <pre className="text-[15px]">
+            {output.map((output) => <div key={output.id} className="h-full w-full font-JetBrainsMono">
+              <p>
+                {output.prefix}
+                <span className="text-[#cbc57e]">
+                  {output.command}
+                </span>
+              </p>
+              <div>
+                {output.response}
+              </div>
+            </div>)}
+          </pre>
+        </div>
+        <div className="flex">
+        <pre className="text-[15px] font-JetBrainsMono">
+          <p>
+            <Prefix/>
+          </p>
         </pre>
-      </div>
-      <div className="flex">
-      <pre className="text-[15px] font-JetBrainsMono">
-        <p>
-          <Prefix/>
-        </p>
-      </pre>
-        <input type="text" value={input} onChange={e=>{setInput(e.target.value) 
-        if(commands.indexOf(e.target.value) > -1){setInputColor("#3fc1ee")}else{setInputColor("#ef6d6a")} }} onKeyDown={e=>{
+          <input type="text w-full" ref={inputRef} value={input} onChange={e=>{setInput(e.target.value) 
+          if(commands.indexOf(e.target.value) > -1){setInputColor("#3fc1ee")}else{setInputColor("#ef6d6a")} }} onKeyDown={e=>{
 
-          if (e.key === "Enter") {
-            newOutput.prefix = <Prefix/>
-            newOutput.command = input
-            newOutput.id = output.length
-            
-            switch(input){
-              case commands[0]:
-                newOutput.response = <Help/>
-                break;
-              case commands[1]:
-                clear = true
-                break;
-              default:
-                newOutput.response = ""
+            if (e.key === "Enter") {
+              newOutput.prefix = <Prefix/>
+              newOutput.command = input
+              newOutput.id = output.length
+
+              switch(input){
+                case commands[1]:
+                  newOutput.response = <Help/>
+                  break;
+                case commands[2]:
+                  clear = true
+                  break;
+                default:
+                  newOutput.response = ""
+              }
+
+              setInput("")
+              setInputColor("#3fc1ee")
+
+              if(clear !== true){
+                setOutput(oldOutput => [...oldOutput, newOutput]);
+              }else{
+                setOutput([])
+              }
             }
-
-            setInput("")
-            setInputColor("#3fc1ee")
-
-            if(clear !== true){
-              setOutput(oldOutput => [...oldOutput, newOutput]);
-            }else{
-              setOutput([])
-            }
-          }
-        }} className={`bg-transparent text-[${inputColor}] outline-none m-0 p-0 text-[15px] font-JetBrainsMono`}/>
+          }} className={`bg-transparent text-[${inputColor}] outline-none m-0 p-0 text-[15px] font-JetBrainsMono`}/>
+        </div>
+        <div ref={autoScrollRef} />
       </div>
     </div>
   );
